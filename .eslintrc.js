4bd7488a81840deeda83f29904e7f080b40b1b1c
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-07-20 23:51:10 +0900

    chore: git init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['@ken0x0a/eslint-config'],
+  rules: {
+    'no-underscore-dangle': 0,
+    // 'react/no-array-index-key': 0,
+    'react-native/no-inline-styles': 0,
+    '@typescript-eslint/camelcase': [
+      'error',
+      /**
+       * - m: minus
+       * - p: plus
+       * - d: divide
+       * - t: times (multiply)
+       */
+      { allow: ['^UNSAFE_', '_m\\d+$', '_p\\d+$', '_d\\d+$', '_t\\d+$'] },
+    ],
+    '@typescript-eslint/no-unused-vars': 0, // typescript has better checking
+  },
+}
