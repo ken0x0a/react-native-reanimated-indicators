796ff91bc360d095e14ee64ae17682be9cafa2f3
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-07-21 01:41:35 +0900

    chore: update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
     'no-underscore-dangle': 0,
     // 'react/no-array-index-key': 0,
     'react-native/no-inline-styles': 0,
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/array-type': 0,
     '@typescript-eslint/camelcase': [
       'error',
       /**
@@ -15,5 +17,6 @@ module.exports = {
       { allow: ['^UNSAFE_', '_m\\d+$', '_p\\d+$', '_d\\d+$', '_t\\d+$'] },
     ],
     '@typescript-eslint/no-unused-vars': 0, // typescript has better checking
+    '@typescript-eslint/no-use-before-define': 0, // typescript has better checking
   },
 }
