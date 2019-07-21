f9de183738a6e514fabce3c06483987a5edccdb6
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-07-21 16:28:31 +0900

    chore: bump devDeps eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
   extends: ['@ken0x0a/eslint-config'],
   rules: {
-    'no-underscore-dangle': 0,
-    // 'react/no-array-index-key': 0,
-    'react-native/no-inline-styles': 0,
-    '@typescript-eslint/indent': 0,
-    '@typescript-eslint/array-type': 0,
     '@typescript-eslint/camelcase': [
       'error',
       /**
@@ -16,7 +11,5 @@ module.exports = {
        */
       { allow: ['^UNSAFE_', '_m\\d+$', '_p\\d+$', '_d\\d+$', '_t\\d+$'] },
     ],
-    '@typescript-eslint/no-unused-vars': 0, // typescript has better checking
-    '@typescript-eslint/no-use-before-define': 0, // typescript has better checking
   },
 }
